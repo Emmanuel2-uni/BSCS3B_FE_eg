d6dd9e6c730d16728ff774adc8a15b02fad09419
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2024-11-19 15:15:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+/* 
+
+console.log(fetch('https://pbs.twimg.com/media/GKhGFH_WwAEsGwy.jpg').then(response => {
+  console.log(response)  
+  return response.blob()
+}).then(blob =>{
+    console.log(blob)
+    document.querySelector('#myImg').src=URL.createObjectURL(blob)
+}).catch(error =>{
+    console.log(error)
+}))
+
+*/
+
+
+
+const content = document.querySelector('#content')
+
+window.addEventListener("load", () => {
+  getUsers();
+})
+
+function getUsers(){
+  
+  let html = ""
+  fetch("http://localhost:5002/api/members", {mode: "cors"})
+
+    .then((response)=>{
+      console.log(response)
+      return response.json();
+    })
+
+    .then((data) =>{
+      data.forEach((element)=>{
+        html += `<li> ${element.first_name} ${element.last_name}</li>`
+      })
+      content.innerHTML = html
+    })
+
+    .catch((error) =>{
+      console.log(error)
+    })
+}
